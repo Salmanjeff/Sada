8547a7fb63b421cefc24ab004b660fa49b495391
Salmanjeff <90747094+Salmanjeff@users.noreply.github.com>
2021-09-16 14:36:19 -0400

    Update profitvalue.py

--- profitvalue.py
+++ profitvalue.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv (r'C:\Users\Hira- Personal\Desktop\data.csv')
+df = pd.read_csv (r'C:\Users\mypc\Desktop\data.csv')
 index=df.index
 numberofrows=len(index)
 print('First Printed Answer:\nTotal Number of rows are=',numberofrows)
@@ -8,7 +8,7 @@ adf=df[pd.to_numeric(df['Profit (in millions)'], errors='coerce').notnull()]
 index=adf.index
 numberofrows=len(index)
 
-adf.to_json (r'C:\Users\Hira- Personal\Desktop\data2.json')
+adf.to_json (r'C:\Users\mypc\Desktop\data2.json')
 print('\nSecond Printed Answer:\nTotal Number of rows after removing\nnon-numeric values from profit coloums are=',numberofrows)
 
 print('\nThird Printed Answer:\nlist of First 20 vlaues by Max profit')
